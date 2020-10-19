a9b108093f1a51c50e98bd3c24098a7c3f7be875
Leonardo <leonardonanci@id.uff.br>
2020-10-19 17:05:35 -0300

    Header dict

--- parser.py
+++ parser.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 
 class Parser(BeautifulSoup):
 
+    __header__ = None
+
     def __init__(self, content):
         super().__init__(content, 'html.parser')
 
@@ -17,7 +19,26 @@ class Parser(BeautifulSoup):
         for imgTag in self.find_all('img'):
             imgSrc = imgTag.get('src')
             imgs.append(imgSrc)
-            # faz requisição
-            # print(imgSrc)
 
         return imgs
+
+    # Retorna a header em forma de dicionário
+    def getHeader(self):
+        if self.__header__ == None:
+
+            header_dict = {}
+            header_str = str(self).split('<!DOCTYPE HTML>')[0]
+            lines = header_str.split('\r\n')
+
+            # Código de status
+            header_dict['Status'] = int(lines[0].split()[1])
+
+            # Outras infos
+            for line in lines[1:]:
+                if line:
+                    key, val = line.split(': ')
+                    header_dict[key] = val
+
+            self.__header__ = header_dict
+
+        return self.__header__
